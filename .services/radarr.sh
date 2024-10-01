2aa85888f7941fe5a14c7dc7d424a8a8c38a4f72
ByteBlend <127003436+byteblenduk@users.noreply.github.com>
2024-10-01 04:09:03 +0100

    Update radarr.sh

--- .services/radarr.sh
+++ .services/radarr.sh
@@ -5,12 +5,19 @@ cat >> "$DOCKER_COMPOSE_FILE" <<EOL
 
   radarr:
     container_name: radarr
-    image: ghcr.io/hotio/radarr
+    image: lscr.io/linuxserver/radarr:latest
     environment:
-      - PUID=1000
-      - PGID=1000
-      - UMASK=002
-      - TZ=Europe/London
+      - PUID=\${PUID}
+      - PGID=\${PGID}
+      - TZ=\${TZ}
     volumes:
-      - ./.appdata/radarr/config:/config
-      - /data:/data
+      - \${APPDATA_DIR}/radarr/config:/config
+      - \${DATA_DIR}:/data
+    labels:
+      - "traefik.enable=true"  # Enable Traefik for this service
+      - "traefik.http.routers.radarr.rule=Host(`radarr.\${BASE_DOMAIN}`)"  # Router rule for domain
+      - "traefik.http.routers.radarr.entrypoints=websecure"  # Use the secure entrypoint
+      - "traefik.http.routers.radarr.tls=true"  # Enable TLS
+      - "traefik.http.routers.radarr.tls.certresolver=myresolver"  # Use the Let's Encrypt resolver
+      - "traefik.http.services.radarr.loadbalancer.server.port=7878"  # Radarr's internal port
+    restart: unless-stopped
