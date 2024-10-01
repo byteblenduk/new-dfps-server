7993507cf03601640216409de89b23d021ef5180
ByteBlend <127003436+byteblenduk@users.noreply.github.com>
2024-10-01 04:09:56 +0100

    Update radarr.sh

--- .services/radarr.sh
+++ .services/radarr.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+# Check if the script is run by setup.sh
+if [ "$RUN_BY_SETUP" != "true" ]; then
+  echo "This script must be run by setup.sh" 1>&2
+  exit 1
+fi
+
+# User home directory (use tilde expansion for flexibility)
+USER_HOME=$(eval echo "~$USERNAME")
+
 # Radarr service configuration
 cat >> "$DOCKER_COMPOSE_FILE" <<EOL
 
